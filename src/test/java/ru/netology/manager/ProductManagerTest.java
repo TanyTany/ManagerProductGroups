98e1d0edb4c82d15c447d0966289ac1e2801b5de
TanyTany <avdtatyana@gmail.com>
2021-01-31 12:35:31 +0300

    added tests

--- src/test/java/ru/netology/manager/ProductManagerTest.java
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ProductManagerTest {
     @Test
-            public void add() {
+            public void shouldGetItems() {
         ProductManager manager = new ProductManager();
         Book first = new Book(1, "NameBook1", 750, "author1");
         Book second = new Book(2, "NameBook2", 750, "author1");
@@ -24,6 +24,33 @@ class ProductManagerTest {
 
         assertArrayEquals(actual, expected);
     }
+
+    @Test
+    public void shouldGetIfNotItems() {
+        ProductManager manager = new ProductManager();
+
+
+        Product[] actual = manager.repository.findAll();
+        Product[] expected = new Product[] {};
+
+        assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldGetIfOneItems() {
+        ProductManager manager = new ProductManager();
+        Book first = new Book(1, "NameBook1", 750, "author1");
+
+        manager.add(first);
+
+
+        Product[] actual = manager.repository.findAll();
+        Product[] expected = new Product[] {first};
+
+        assertArrayEquals(actual, expected);
+    }
+
+
 
 }
 
